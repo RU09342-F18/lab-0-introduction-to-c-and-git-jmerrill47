938398934b31ff5a176bd6c9c27f31559aeef99c
jmerrill47 <36243046+jmerrill47@users.noreply.github.com>
2018-09-10 14:08:57 -0400

    Update math.h

--- Assignment/math.h
+++ Assignment/math.h
@@ -26,4 +26,9 @@
 //Part of your documentation should be listing the valid inputs and outputs for the functions you create.
 int math(int num1, int num2, char Operator);
 
+//Global Variables
+int num1;
+int num2;
+char Operator;
+
 #endif /* MATH_H_ */
